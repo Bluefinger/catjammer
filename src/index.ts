eab8d4c75233a475e1fd72ae006f3b37613313c1
Stuart Payne <paynestu@gmail.com>
2020-09-23 20:06:36 +0100

    help

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+const { token: string, prefix: string } = require("../config.json");
+const Discord: any = require("discord.js");
+
+const client: Client = new Discord.Client();
+
 console.log("Here be bot");
