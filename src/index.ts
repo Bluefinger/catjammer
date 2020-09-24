3a7356c02bca490ac3e5d2cc0d8daec294458aea
stuart-payne <52239906+stuart-payne@users.noreply.github.com>
2020-09-24 15:46:14 +0100

    Command handler (#4)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("Here be bot");
+import Discord from "discord.js";
+import * as commands from "./commands";
+
+/* eslint-disable-next-line */
+const { token, prefix } = require("../config.json") as { token: string; prefix: string };
+
+const client = new Discord.Client();
+const commandCollection = new Discord.Collection(Object.entries(commands));
+
+client.once("ready", () => {
+  console.log("ready");
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const arg = args.shift();
+  if (arg) {
+    const command = arg.toLowerCase();
+
+    if (!commandCollection.has(command)) return;
+
+    try {
+      commandCollection.get(command)?.execute(message, args);
+    } catch (error) {
+      console.error(error);
+      void message.reply("there was an error executing that command");
+    }
+  }
+});
+
+void client.login(token);
