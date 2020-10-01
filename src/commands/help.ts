2fc1979da9e19dd2b3efc8d4cc3a5a5622e9529f
Stuart Payne <paynestu@gmail.com>
2020-10-01 16:55:23 +0100

    improved help strings

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -4,7 +4,7 @@ export const help: Command = {
   name: "help",
   description: "provides instruction on how to call commands",
   definition: "help :command",
-  help: "",
+  help: "use !help <>, replacing the brackets with the name of the command",
   async execute({ message, args, commands }): Promise<void> {
     const commandName = args["command"];
     const result = commands.find((command) => command.name === commandName);
