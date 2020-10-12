7bd0491b6e5f9d72aedf0cca8253b1d23f9e1473
Stuart Payne <paynestu@gmail.com>
2020-10-12 12:01:48 +0100

    removal of reschedule

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -5,6 +5,5 @@ import { help } from "./help";
 import { Command } from "./type";
 import { color } from "./color";
 import { cancel } from "./cancel";
-import { reschedule } from "./reschedule";
 
-export const commands: Command[] = [schedule, ping, commandsList, color, help, cancel, reschedule];
+export const commands: Command[] = [schedule, ping, commandsList, color, help, cancel];
